27fd5e0c454fbefbd109b7b88d82380cac9acb54
leonardomsoares1 <leonardomsoares1@gmail.com>
2025-08-24 07:55:43 -0300

    fix: corrige exportação no arquivo index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export * from './queues';
-export * from './events';
-export * from './envelope';
+export { QUEUES } from './queues';
+export type { EventName, QueueName } from './queues';
+
+export { ETypeEventStatus } from './events';
+export type {
+  EventData,
+  IActorMeta,
+  IEventDataByName,
+  IEventTypeSnapshot,
+  ILogTypeEventCreateData,
+  ILogTypeEventDisableData,
+  ILogTypeEventEnableData,
+  ILogTypeEventUpdateData,
+} from './events';
+
+export type { IEventEnvelope } from './envelope';
