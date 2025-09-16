7e32ae3217ba6da5c77927453fef366b9ec53fbd
leonardosoares1 <leonardo.soares@racsys.com.br>
2025-09-16 16:46:50 -0300

    fix: ajuste no snapshot de instituição

--- src/events/institutions.ts
+++ src/events/institutions.ts
@@ -1,7 +1,22 @@
 import { IActorMeta } from './shared.js';
 
 export interface IInstitutionSnapshot {
+  addressCity: string;
+  addressComplement: string | null;
+  addressNeighborhood: string;
+  addressNumber: string;
+  addressPostalCode: string;
+  addressState: string;
+  addressStreet: string;
+  cnpj: string;
+  corporateName: string;
+  email: string;
+  fantasyName: string;
   id: string;
+  passwordHash: string;
+  phone: string;
+  responsibleName: string;
+  responsibleRole: string;
 }
 
 export interface ILogInstitutionCreateByAgencyData extends IActorMeta {
